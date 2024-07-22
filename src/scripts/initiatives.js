1fbedd7efc3463ff831e27d4b749b533a7572bff
Kegan Maher <kegan@compiler.la>
2024-07-22 22:25:41 +0000

    refactor(script): initiative pills change hash
    update the URL so it matches the initiative the user is looking at
    
    logic that shows a pill's content is now tied to the hash being changed
    
    inserts initiative content into user's history

--- src/scripts/initiatives.js
+++ src/scripts/initiatives.js
@@ -1,16 +1,25 @@
 window.addEventListener("load", () => {
   const triggerTabList = document.querySelectorAll("#pills-tab button");
-  triggerTabList.forEach((triggerEl) => {
-    const tabTrigger = new bootstrap.Tab(triggerEl);
 
+  triggerTabList.forEach((triggerEl) => {
     triggerEl.addEventListener("click", (event) => {
       event.preventDefault();
-      tabTrigger.show();
+      // update the window.location.hash with the clicked pill's target
+      window.location.hash = event.target.getAttribute("data-bs-target").replace("#", "");
     });
   });
 
-  if (window.location.hash.startsWith("#initiatives")) {
-    const triggerEl = document.querySelector("button[data-bs-target='" + window.location.hash + "']");
-    bootstrap.Tab.getInstance(triggerEl).show();
+  // watch for changes to the window.location.hash value
+  function checkInitiativesHash() {
+    if (window.location.hash.startsWith("#initiatives")) {
+      const triggerEl = document.querySelector("button[data-bs-target='" + window.location.hash + "']");
+      const tabTrigger = new bootstrap.Tab(triggerEl);
+      tabTrigger.show();
+    }
   }
+
+  window.onhashchange = checkInitiativesHash;
+
+  // call the function once on page load to check for the initial hash
+  checkInitiativesHash();
 });
