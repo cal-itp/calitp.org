68487afe0772b5e505738e6a6457e436e4b4fc61
Machiko Yasuda <machiko@compiler.la>
2024-08-01 16:15:58 +0000

    fix(sidebar): add remove hash line, remove set hash line

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -4,18 +4,19 @@ window.addEventListener("load", () => {
   sidebarLinks.forEach((link) => {
     link.addEventListener("click", (clicked) => {
       sidebarLinks.forEach((link) => link.classList.remove("active"));
-      // update the window.location.hash with the clicked link's anchor target
-      window.location.hash = clicked.target.attributes.href.nodeValue;
     });
   });
 
   // watch for changes to window.location.hash value that start with #stories
   // add active class on the sidebar target anchor
+  // else, reset the table of contents
   function checkStoriesHash() {
     if (window.location.hash.startsWith("#stories")) {
       sidebarLinks.forEach((link) => link.classList.remove("active"));
       linkPair = document.querySelectorAll("a[href='" + window.location.hash + "']");
       linkPair.forEach((link) => link.classList.add("active"));
+    } else if (window.location.hash == "") {
+      sidebarLinks.forEach((link) => link.classList.remove("active"));
     }
   }
 
