9819e2ff44e4200ef35a9c139286d15519eaac5d
John Gravois <jgravois@compiler.la>
2026-04-11 14:00:54 +0000

    chore: try to force clear pre-commit.ci cache

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
-// this convoluted plugins syntax is needed to ensure the plugin is available in pre-commits context
+// this convoluted plugins syntax is needed to ensure it is available in the pre-commit context specifically
 // if the shopify plugin was ESM compatible, we could use await import() instead
 // https://stackoverflow.com/questions/78708497/prettier-plugins-not-found-with-pre-commit
 // https://github.com/prettier/prettier/issues/15085
 const config = {
-  plugins: [import.meta.resolve("@shopify/prettier-plugin-liquid")],
   printWidth: 130,
+  plugins: [import.meta.resolve("@shopify/prettier-plugin-liquid")],
 };
 
 export default config;
