58674d71018d6ac28180bb85c00120b9f222f98c
John Gravois <jgravois@compiler.la>
2026-04-11 13:20:03 +0000

    chore: simplify prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,9 @@
-// if the plugin was ESM compatible, we could use await import('@shopify/prettier-plugin-liquid') instead of shimming in require
-// https://stackoverflow.com/questions/69099763/referenceerror-require-is-not-defined-in-es-module-scope-you-can-use-import-in
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-// Allow Prettier in .pre-commit-config.yaml to find plugins.
+// this convoluted plugins syntax is needed to ensure the plugin is available in pre-commits context
+// if the shopify plugin was ESM compatible, we could use await import() instead
 // https://stackoverflow.com/questions/78708497/prettier-plugins-not-found-with-pre-commit
 // https://github.com/prettier/prettier/issues/15085
 const config = {
-  plugins: [require.resolve("@shopify/prettier-plugin-liquid")],
+  plugins: [import.meta.resolve("@shopify/prettier-plugin-liquid")],
   printWidth: 130,
 };
 
