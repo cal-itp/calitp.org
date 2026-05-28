52436074ab98bf419046391691ab7bfea8968887
John Gravois <jgravois@compiler.la>
2026-05-28 13:19:25 +0000

    fix: update timestamp in resource template

--- .github/scripts/resource-template.py
+++ .github/scripts/resource-template.py
@@ -180,7 +180,7 @@ def main():
   tags = issue['tag'] if isinstance(issue['tag'], list) else [issue['tag']]
   content = f"""
 ---
-date: "{issue['date_year']}-{issue['date_month']}-01 00:00:00"
+date: "{issue['date_year']}-{issue['date_month']}-01T00:00:00"
 title: "{issue['title']}"
 asset: "{issue['asset_url']}"
 category: "{issue['category']}"
